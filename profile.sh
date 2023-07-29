337fdc0e58ef14db291c2f71722387625bf8ca05
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-07-30 02:15:12 +0900

    Fix

--- profile.sh
+++ profile.sh
@@ -5,4 +5,4 @@ alias nv='nvim'
 alias cls='clear'
 
 export TERM=xterm-256color
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
