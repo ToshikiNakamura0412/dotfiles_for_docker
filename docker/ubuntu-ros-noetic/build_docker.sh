cf9f411e8c80d03558dede43f5cc2f88bb9bf43d
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-08-10 17:45:05 +0900

    Update docker for ros

--- docker/ubuntu-ros-noetic/build_docker.sh
+++ docker/ubuntu-ros-noetic/build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t test-dotfiles-for-ros --no-cache=true .
+docker build -t test-dotfiles-for-ros .
--- docker/ubuntu-ros-noetic/run_docker.sh
+++ docker/ubuntu-ros-noetic/run_docker.sh
@@ -3,14 +3,14 @@
 # SCRIPT_DIR=$(cd $(dirname $0); pwd)
 # DOTFILES_DIR="$HOME/dotfiles_for_docker"
 USER="nakamura"
-WORK_DIR="$HOME/code"
+WORK_DIR="$HOME/catkin_ws/src/<pkg-name>"
 docker run \
     --ipc=host \
     --net=host \
     --rm \
     -it \
     --privileged \
-    -v $WORK_DIR:/home/$USER/workdir \
+    -v $WORK_DIR:/root/catkin_ws/src/<pkg-name> \
     test-dotfiles-for-ros \
     bash \
     --login
