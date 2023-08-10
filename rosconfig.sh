5160af6e6561d75d1375221c7d01edcd46545828
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-08-10 17:12:15 +0900

    Fix

--- rosconfig.sh
+++ rosconfig.sh
@@ -1,12 +1,6 @@
 # Set ROS Noetic
-if [ $0 = "bash" ]; then # bash
-    source /opt/ros/noetic/setup.bash
-    source ~/catkin_ws/devel/setup.bash
-else # zsh
-    source /opt/ros/noetic/setup.zsh
-    source ~/catkin_ws/devel/setup.zsh
-fi
-# common
+source /opt/ros/noetic/setup.bash
+source ~/catkin_ws/devel/setup.bash
 export ROS_WORKSPACE=~/catkin_ws
 export ROS_PACKAGE_PATH=~/catkin_ws/src:$ROS_PACKAGE_PATH
 export ROSCONSOLE_FORMAT='[${severity}] [${time}] [${node}]: ${message}'
