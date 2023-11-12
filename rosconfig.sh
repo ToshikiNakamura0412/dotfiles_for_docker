da312e287c72e9cad003c67467da4bf26c2e59d6
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2023-11-12 21:09:22 +0900

    Update path (#3)
    * Update vimrc
    
    * Update path

--- rosconfig.sh
+++ rosconfig.sh
@@ -1,7 +1,7 @@
 # Set ROS Noetic
 source /opt/ros/noetic/setup.bash
-source ~/catkin_ws/devel/setup.bash
-export ROS_WORKSPACE=~/catkin_ws
-export ROS_PACKAGE_PATH=~/catkin_ws/src:$ROS_PACKAGE_PATH
+source $HOME/catkin_ws/devel/setup.bash
+export ROS_WORKSPACE=$HOME/catkin_ws
+export ROS_PACKAGE_PATH=$HOME/catkin_ws/src:$ROS_PACKAGE_PATH
 export ROSCONSOLE_FORMAT='[${severity}] [${time}] [${node}]: ${message}'
 export EDITOR='nvim'
