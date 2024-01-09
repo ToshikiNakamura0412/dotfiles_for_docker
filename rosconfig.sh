e95162d8b2acd80446ee0bfdda2d2b10465c4381
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-01-09 11:07:04 +0900

    Add docker for ROS1 Noetic (#4)

--- rosconfig.sh
+++
@@ -1,7 +0,0 @@
-# Set ROS Noetic
-source /opt/ros/noetic/setup.bash
-source $HOME/catkin_ws/devel/setup.bash
-export ROS_WORKSPACE=$HOME/catkin_ws
-export ROS_PACKAGE_PATH=$HOME/catkin_ws/src:$ROS_PACKAGE_PATH
-export ROSCONSOLE_FORMAT='[${severity}] [${time}] [${node}]: ${message}'
-export EDITOR='nvim'
