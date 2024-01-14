5c62a7b85a15d5ddef9d34cec85495bedaf749e2
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-01-15 04:01:32 +0900

    Add profile settings (#18)

--- profile.sh
+++ profile.sh
@@ -1,7 +1,10 @@
-alias ll='ls -l'
-alias la='ls -lA'
-alias l='ls -la'
-alias nv='nvim'
-alias cls='clear'
+source /etc/os-release
+OS_NAME=$(echo $ID)
 
-export TERM=xterm-256color
+if [ $OS_NAME = "alpine" ]; then
+    alias ll='ls -l'
+    alias la='ls -lA'
+    alias l='ls -la'
+fi
+
+export TERM=screen-256color
