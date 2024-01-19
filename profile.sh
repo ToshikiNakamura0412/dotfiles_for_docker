35f3ea319ab819ae2fd1ef9d3e1c0eaabeed4061
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-01-19 23:50:53 +0900

    Add aliases (#22)

--- profile.sh
+++ profile.sh
@@ -5,6 +5,30 @@ if [ $OS_NAME = "alpine" ]; then
     alias ll='ls -l'
     alias la='ls -lA'
     alias l='ls -la'
-fi
+elif [ $OS_NAME = "ubuntu" ]; then
+    # Set basic alias commands
+    alias cw='cd $ROS_WORKSPACE'
+    alias cs='cd $ROS_WORKSPACE/src'
+    alias cb='catkin build -DCMAKE_BUILD_TYPE=RelWithDebInfo'
+    alias cbt='catkin build --this -DCMAKE_BUILD_TYPE=RelWithDebInfo'
 
-export TERM=screen-256color
+    # Set alias commands for ROS completion and build
+    function create-simple-compile-flags() {
+        echo "-I/opt/ros/$ROS_DISTRO/include" > compile_flags.txt
+        echo "-I$PWD/include" >> compile_flags.txt
+    }
+    function cbt-export-compile-commands() {
+        catkin build --this -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+        echo ""
+        if [ -e package.xml ]; then
+            local package_name=$(cat package.xml | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
+            if [ -e $ROS_WORKSPACE/build/$package_name/compile_commands.json ]; then
+                ln -sfv $ROS_WORKSPACE/build/$package_name/compile_commands.json ./compile_commands.json
+            else
+                echo "Error: compile_commands.json not found in build directory"
+            fi
+        else
+            echo "Error: package.xml not found in current directory, so can't create symlink to compile_commands.json"
+        fi
+    }
+fi
